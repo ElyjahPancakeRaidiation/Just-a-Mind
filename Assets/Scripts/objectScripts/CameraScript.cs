09460b4e9588bf0d20f5d51e4489591893100021
Elyjah <ely.loan2006@gmail.com>
2024-01-20 14:58:29 -0500

    Camera script

--- Assets/Scripts/objectScripts/CameraScript.cs
+++ Assets/Scripts/objectScripts/CameraScript.cs
@@ -5,18 +5,17 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-
-    public bool firstCam;
-    public bool secondCam;
-    [Space(50)]
     private GameObject cameraObj;
     public static GameObject playerObj;
+    private Rigidbody2D playerRB;
     public float cameraSpeed;
+
+    private bool goingRight, goingUp;//If player is going left, right will be false if player is going down, up will be false
 
     [SerializeField]public bool isFollowingPlayer;
     [SerializeField]private Vector2 cameraOffset;
-    private Vector2 playerLimit;//When the player passes this vec2 it will start to increase in the speed
-
+    private Vector2 startCamOffset; 
+    //public float yIncreaser;
 
     public static bool isCameraShaking, isCameraZooming;
     [SerializeField, Range(0, 3)]private float maxCameraShakePosX, maxCameraShakePosY;
@@ -34,74 +33,100 @@ public class CameraScript : MonoBehaviour
         camDefaultValues.camFOV = 5;
         cameraObj = GameObject.FindGameObjectWithTag("MainCamera");
         playerObj = GameObject.FindGameObjectWithTag("Player");
+        playerRB = playerObj.GetComponent<Rigidbody2D>();
+        startCamOffset = cameraOffset;
 
 
     }
 
     private void Update(){
         CameraShake();      
-
-        if (isFollowingPlayer)
-        {
-            /*
-            if(playerObj.GetComponent<Rigidbody2D>().velocity.x != 0){
-                cameraOffset.x = playerObj.GetComponent<PlayerController>().horizontal;
-            }
-            */
-        }
+        
     }
     private void FixedUpdate() {
         if (isFollowingPlayer)
         {
-            if (firstCam)
-            {
-                FollowObjDelay(cameraSpeed);
-            }
-            
-            if (secondCam)
-            {
-                //FollowPlayer(cameraSpeed);
-            }
+            FollowObjDelay(cameraSpeed);
         }
     }
 
     public void FollowObjDelay(float speed)//Follows any obj(Should always be put in fixed update so it can add the rigidbody. If it is not in F.U it will make anything with a rigidbody jitter when moved.)
     {
+        transform.position = Vector2.Lerp(transform.position, (Vector2)playerObj.transform.position + cameraOffset, speed);
+        GoingToFast(40, 40);
+
+    }
+
+    private void GoingToFast(float maxLimitX, float maxLimitY){
+        //Should increase the offset in the direction its going to fast in.
+        if (playerRB.velocity.x > maxLimitX)
+        {
+            print("Going Right");
+            goingRight = true;
+            cameraOffset.x += Time.deltaTime / playerRB.velocity.x;
+            
+        }else if(playerRB.velocity.x < -maxLimitX){
+            print("Going Left");
+            goingRight = false;
+            cameraOffset.x += Time.deltaTime / playerRB.velocity.x;
+        }
         /*
-        Vector2 targetPosition = new Vector2(objTransform.position.x, objTransform.position.y);
-        transform.position = Vector2.SmoothDamp(transform.position, targetPosition + cameraOffset, ref camVel, speed);//With smoothDamp the lower the speed the faster it goes also itt's smoother than Lerp.
-        //We can also cap the speed it can go at with smoothDamp
+        if (playerRB.velocity.y > maxLimitY)
+        {
+            print("Going Up");
+            cameraOffset.y += Time.deltaTime / playerRB.velocity.y;
+            goingUp = true;
+        }else if(playerRB.velocity.y < -maxLimitY){
+            print("Going Down");
+            goingUp = false;
+            cameraOffset.y += -(Time.deltaTime * yIncreaser);
+        }
         */
 
-        transform.position = Vector2.Lerp(transform.position, playerObj.transform.position, speed);
-
-    }
-
-    private void IncreaseSpeed(){
-        Vector2 playerCamPos = Camera.main.ScreenToWorldPoint(playerObj.transform.position);
-        if(playerCamPos.x < -playerLimit.x){
-
-        }else if(playerCamPos.x > playerLimit.x){
-
-        }else if(playerCamPos.y < -playerLimit.y){
-
-        }else if(playerCamPos.y > playerLimit.y){
-            
-        }
-    }
-
-
-
-    private void LowerNum(ref float startingNum, float endNum, float decreasePower){
-        if (startingNum > endNum)
+        if (goingRight && playerRB.velocity.x < maxLimitX)
         {
-            startingNum -= decreasePower * Time.deltaTime;
+            if (cameraOffset.x > startCamOffset.x)
+            {
+                cameraOffset.x -= Time.deltaTime / playerRB.velocity.x;
+            }
+            else
+            {
+                cameraOffset.x = startCamOffset.x;
+            }
+        }else if(!goingRight && playerRB.velocity.x > -maxLimitX){
+            if (cameraOffset.x < startCamOffset.x)
+            {
+                cameraOffset.x -= Time.deltaTime / playerRB.velocity.x;
+            }
+            else
+            {
+                cameraOffset.x = startCamOffset.x;
+            }
         }
 
-        if (startingNum < endNum)
+        /*
+        if (goingUp && playerRB.velocity.y < maxLimitY)
         {
-            startingNum = endNum;
+            if (cameraOffset.y > startCamOffset.y)
+            {
+                cameraOffset.y -= Time.deltaTime / playerRB.velocity.y;
+            }
+            else
+            {
+                cameraOffset.y = startCamOffset.y;
+            }
+        }else if(!goingUp && playerRB.velocity.y > -maxLimitY){
+            if (cameraOffset.y < startCamOffset.y)
+            {
+                cameraOffset.y -= Time.deltaTime / playerRB.velocity.y;
+            }
+            else
+            {
+                cameraOffset.y = startCamOffset.y;
+            }
         }
+        */
+
     }
 
     public void ZoomCameraChange(float FOV, float zoomSpeed){//Zooms back and fourth wether it is the player or not. Never make the desired FOV smaller than the defualt FOV which is 5
@@ -114,7 +139,9 @@ public class CameraScript : MonoBehaviour
             }else{
                 cameraObj.GetComponent<Camera>().orthographicSize = FOV;
             }
-        }else{
+        }
+        else
+        {
             if (cameraObj.GetComponent<Camera>().orthographicSize > camDefaultValues.camFOV)
             {
                 cameraObj.GetComponent<Camera>().orthographicSize -= Time.deltaTime * zoomSpeed;
